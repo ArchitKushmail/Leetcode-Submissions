8dca10bd6d3107c393813724089ece8eaf36a3b8
ArchitKushmail <68703970+ArchitKushmail@users.noreply.github.com>
2023-10-08 23:58:10 +0530

    Time: 2 ms (59.08%) | Memory: 7 MB (22.12%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+	public:
+		bool checkIfPangram(string sentence)
+		{
+			if (sentence.size() < 26) return false;
+			unordered_map<char, int> ans;
+			for (int i = 0; i < sentence.length(); i++)
+			{
+				ans[sentence[i]]++;
+			}
+
+			if (ans.size() == 26)
+			{
+				return true;
+			}
+
+			return false;
+		}
+};
