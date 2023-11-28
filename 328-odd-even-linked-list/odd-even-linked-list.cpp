cfb1f8181bfd8bf3f9fc7e5fb380465805735145
ArchitKushmail <68703970+ArchitKushmail@users.noreply.github.com>
2023-11-28 22:33:50 +0530

    Time: 6 ms (83.91%) | Memory: 10.8 MB (72.21%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,29 +11,22 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-    
-       if (!head || !head->next || !head->next->next) {
-        // If the list has 0, 1, or 2 nodes, return the list as it is
+    if (!head || !head->next || !head->next->next) {
+        
         return head;
     }
-
-    ListNode* odd = head; // Points to the head of odd nodes
-    ListNode* even = head->next; // Points to the head of even nodes
-    ListNode* evenHead = even; // Store the head of the even nodes
-
-    while (even != nullptr && even->next != nullptr) {
-        // Reorder the odd nodes
-        odd->next = even->next;
-        odd = odd->next;
-
-        // Reorder the even nodes
-        even->next = odd->next;
-        even = even->next;
-    }
-
-    // Merge odd nodes and even nodes
-    odd->next = evenHead;
-
-    return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+        while(even!=nullptr && even->next!=nullptr){
+            odd->next = even->next;
+            odd = odd->next;
+            
+            even->next = odd->next;
+            even = even->next;
+            
+        }
+        odd->next = evenHead;
+        return head;
     }
 };
